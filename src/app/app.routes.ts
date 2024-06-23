c7ae0e961b5ade0f583e382880e80475ed1bacd3
nugrahaa878 <nugrahaa878@gmail.com>
2024-06-23 14:06:23 +0700

    feat: setup routing system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './presenter/pages/home/home.component';
+import { ContactDetailsComponent } from './presenter/pages/contact-details/contact-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'details',
+    component: ContactDetailsComponent,
+  },
+];
